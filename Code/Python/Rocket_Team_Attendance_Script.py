5252cfa479f1faca76baec1e5e19ef6e400e06fe
Liam Normand <norma514@umn.edu>
2023-11-03 11:44:14 -0500

    Fixes to attendance script

--- Code/Python/Rocket_Team_Attendance_Script.py
+++ Code/Python/Rocket_Team_Attendance_Script.py
@@ -5,18 +5,22 @@ def count_attendance(fname):
             full_file = file.readlines()
             for row in full_file:
                 current_row = row.strip().split(',')
-                for n in range(len(current_row)/5):
+                #print(type(current_row))
+                #print(current_row)
+                for n in range(len(current_row)//5):
                     i=5*n+2
-                    full_dict[current_row(i)[0:7].lower()] = full_dict.setdefault(current_row(i)[0:7].lower(), 0) + 1
+                    #print(current_row[i])
+                    full_dict[current_row[i][0:8].lower()] = full_dict.setdefault(current_row[i][0:8].lower(), 0) + 1
+        del full_dict['']
     except FileNotFoundError:
         print("File not found. Check your spelling.")
     
-    for (x500,attendance) in full_dict():
-        print(x500 + str(attendance))
+    for x500, attendance in full_dict.items():
+        print(x500 + ': ' + str(attendance))
     
                 
 if __name__ == '__main__':
-    count_attendance('prop_attendance.csv')           
+    count_attendance("./prop_attendance.csv")          
 
 
 
